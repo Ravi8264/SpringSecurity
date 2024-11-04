b6a414f0503434261df93749d620d96058c9e404
RAVI SHANKAR KUMAR <99506714+Ravi8264@users.noreply.github.com>
2024-11-04 09:05:53 +0530

    Add files via upload

---
+++ exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+//package com.SpringSecurity.SpringSecurity.exceptions;
+//
+//public class ResourceNotFoundException extends RuntimeException{
+//
+//    public ResourceNotFoundException(String message) {
+//        super(message);
+//    }
+//}
---
+++ services/PostService.java
@@ -0,0 +1,16 @@
+//package com.SpringSecurity.SpringSecurity.services;
+//
+//import com.SpringSecurity.SpringSecurity.dto.PostDTO;
+//
+//import java.util.List;
+//
+//public interface PostService {
+//
+//    List<PostDTO> getAllPosts();
+//
+//    PostDTO createNewPost(PostDTO inputPost);
+//
+//    PostDTO getPostById(Long postId);
+//
+//    PostDTO updatePost(PostDTO inputPost, Long postId);
+//}
